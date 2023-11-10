c4d8ffe1714483614a4c95a392725750492ed2cc
Akamiz96 <alejandro.castro2016@outlook.es>
2023-11-10 15:24:03 -0500

    Fix PERL scripts to execute MM1 programs

--- scripts/execution/lanzadorMM1f.pl
+++ scripts/execution/lanzadorMM1f.pl
@@ -3,75 +3,64 @@
 /***************************************************************
 *   Fecha: Octubre 2023
 *   Autor: J. Corredor
+*   Modificado por: Alejandro Castro Martinez
 *   Materia: Computación de Alto Rendimiento
-*   Maestría de Inteligencia Artificial
+*   Maestría en Ingenieria de Sistemas y Computación
 *   Pontificia Universidad Javeriana
 *   Objetivo: Herramienta para automatizar proceso de ejecución
-*             por lotes. 
+*             por lotes para el programa MM1f. 
+*            (Multiplicacion de Matrices por el Algoritmo 
+*            Filas por Filas) 
 ****************************************************************/
 =cut
 
-
+# Verifica si se proporciona un argumento en la línea de comandos
 if(@ARGV[0]){
 		$numRep = "$ARGV[0]";
 }else{
-		usage();
+		usage(); # Llama a la función 'usage' si no se proporciona un argumento
 }
 
+# Imprime el número de repeticiones
 print "\n Repeticiones de la experimentación: $numRep \n\n";
 
 
+# Obtiene el directorio actual y lo asigna a la variable $path0
 $path0 = `pwd`;
 chomp($path0);
+
+# Encuentra la posición de la subcadena "/scripts" en el directorio actual
 $T = index($path0,"/scripts");
+# Obtiene una subcadena desde el inicio hasta la posición encontrada
 $Path = substr($path0,0,$T);
-print "$Path \n";
 
 
-@Ejecutables = ("MM1c"); 
+# Definición de listas de ejecutables, núcleos y tamaños de vector
+@Ejecutables = ("MM1f"); 
 @cores = ("1","2","4","8","10","14","16","20");
 @VectorSize= ("100","200","400","600","800","1000","1500","2000","3000","4000","5000","6000","7000","8000");
 
+# Bucles anidados para iterar sobre ejecutables, tamaños de vector y núcleos
 foreach $exe (@Ejecutables){ 
     foreach $ves (@VectorSize){
         foreach $c (@cores) {
+            # Construye el nombre del archivo de salida
             $file = "$Path/"."Soluciones/"."$exe"."-Size"."$ves"."-core"."$c";
-            #print "$file \n"; #Nombre del fichero
+            # Ejecuta el comando y redirige la salida al archivo
             for($i=0; $i<$numRep;  $i++){
                 system("$Path/BIN/$exe $ves $c 0   >> $file");
                 print "$Path/BIN/$exe $ves $c 0  \n" ;
             }
+            # Se cierra el archivo
             close($file);
         }
     }
 }
+
+# Finaliza el script
 exit(1);
 
-
-sub MedCOV {
-          my($array1, $array2) = @_;
-          @p_vector = @{$array1};
-          @s_vector = @{$array2};
-          $suma  = 0; $std  = 0;
-          $media = 0; $coef = 0;
-          $times   = $s_vector[0];
-          for ($j=0; $j < $times; $j++) {
-              $suma  += $p_vector[$j];
-              $suma2 += $p_vector[$j]**2;
-          }
-          if ($j>0) {
-              $media = $suma/$j;
-          }
-          for ($j=0; $j < $times; $j++) {
-              $std += ($p_vector[$j]-$media)**2;
-          }
-          $cov = sqrt($std/($times-1));
-          if ($media > 0) {
-              $coef = 100*$cov/$media;
-          }
-            splice(@p_vector, 0, $times);
- 	  }
-
+# Subrutina para imprimir mensaje de uso incorrecto y salir del script
 sub usage()
 	{
 		print "\n tst.pl: Uso incorrecto\n\n";
